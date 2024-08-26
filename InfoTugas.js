22aac2303751d15727a0d90304758f1212e0820c
KaanaxD <lmarkabmaulanam348@gmail.com>
2024-08-27 07:25:51 +0800

    Add files via upload

---
+++ InfoTugas.js
@@ -0,0 +1,19 @@
+function senin (){
+    alert('INFO TUGAS\nSKI: Presentasi Video\nFiqih: Outfit Role');
+}
+
+function selasa(){
+    alert('INFO TUGAS\nMAT-W: Ulangan Harian\nEkonomi: Ulangan Harian');
+}
+
+function rabu(){
+    alert('INFO TUGAS\nSEJIN: Pahlawan tiap provinsi\nQurDis: Cerita/Tafsir/Poster');
+}
+
+function kamis(){
+    alert('INFO TUGAS\nMAT-LAN: Kemungkinan Ulangan\nPPKN: Video');
+}
+
+function jumat(){
+    alert('INFO TUGAS\nAQD: Presentasi Video')
+}
